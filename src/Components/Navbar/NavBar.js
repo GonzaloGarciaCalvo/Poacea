3b5ef59efc42618a88d632df4c9cb22e68b1bec0
Gonzalo <garciacalvog@hotmail.com>
2022-07-05 18:23:49 -0300

    ordenamiento, nuevo build

--- src/Components/Navbar/NavBar.js
+++ src/Components/Navbar/NavBar.js
@@ -3,12 +3,12 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
+/* import Typography from '@mui/material/Typography'; */
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
+/* import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb'; */
 import {Link} from 'react-scroll';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import './navbar.css'
@@ -34,7 +34,7 @@ const theme = createTheme({
     }
   },
 });
-const pages = ['Nosotros', 'Galeria', 'Super 8mm', 'Servicios', 'Contacto'];
+/* const pages = ['Nosotros', 'Galeria', 'Super 8mm', 'Servicios', 'Contacto']; */
 
 const NavBar = () => {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
