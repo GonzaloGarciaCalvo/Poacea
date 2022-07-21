4349b599737f11b18f57fd511b6560e483431fda
LopezA93 <voodoo.lux@gmail.com>
2022-07-21 20:09:20 -0300

    form php

--- src/Components/Contacto/Contacto.js
+++ src/Components/Contacto/Contacto.js
@@ -23,7 +23,7 @@ const Contacto = () => {
                 <h1 className="tituloSeccion">CONTACTO</h1>
                 <h3>Explícanos tu proyecto acá y te contestaremos <br/>en breve con una cotización a tu email</h3>
                 <Grid>
-                    <form name="contact"  className="form" method="POST" action="../../../php/formulario.php"  >
+                    <form name="contact"  className="form" method="POST" action="./formulario.php"  >
                         <input type="hidden" name="form-name" value="contact" />
                         {/* <label>Nombre completo</label> */}
                         <input type="text" required placeholder="Ingresa tu nombre" name="name" />
