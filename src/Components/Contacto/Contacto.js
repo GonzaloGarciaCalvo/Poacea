0eba4e37553bdbb0983ae6fd86f5cc50391ab85e
LopezA93 <voodoo.lux@gmail.com>
2022-07-22 16:39:45 -0300

    formulario

--- src/Components/Contacto/Contacto.js
+++ src/Components/Contacto/Contacto.js
@@ -1,10 +1,26 @@
-import { useState, } from "react";
+import { useState, useRef} from "react";
 import { Grid, Container, Button, Modal } from "@mui/material";
 import "../Contacto/style/Contacto.scss"
+import emailjs from '@emailjs/browser';
 
 
 const Contacto = () => {
     const [openModal, setOpenModal] = useState(false)
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_7i15h5p', 'template_o3ggqxd', form.current, 'yqDBror8_hTAX0mCP')
+      .then((result) => {
+          console.log(result.text);
+          setOpenModal(true)
+      }, (error) => {
+          console.log(error.text);
+      });
+      
+  };
 
 
     const addContacto = (e) => {
@@ -23,12 +39,12 @@ const Contacto = () => {
                 <h1 className="tituloSeccion">CONTACTO</h1>
                 <h3>Explícanos tu proyecto acá y te contestaremos <br/>en breve con una cotización a tu email</h3>
                 <Grid>
-                    <form name="contact"  className="form" method="POST" action="/formulario.php"  >
+                    <form name="contact"  className="form" ref={form} onSubmit={sendEmail}  >
                         <input type="hidden" name="form-name" value="contact" />
                         {/* <label>Nombre completo</label> */}
-                        <input type="text" required placeholder="Ingresa tu nombre" name="name" />
+                        <input type="text" required placeholder="Ingresa tu nombre" name="user_name" />
                         {/* <label>Correo Electronico</label> */}
-                        <input type="email" required placeholder="Ingresa tu correo electronico" name="correo" />
+                        <input type="email" required placeholder="Ingresa tu correo electronico" name="user_email" />
                         {/* <label>Ingresa su mensaje</label> */}
                         <textarea required placeholder="Describe tu proyecto aquí" name="message" />
                         <Button type="submit" className="btnContacto" name="button" id="button" variant="contained" >Enviar</Button>
